792da8379f8c912ae339f530cfb422009be3f9f5
Rebecca Turner <me@re-becca.org>
2018-09-04 14:34:58 -0700

    Move to 100% coverage

--- test/queue.js
+++ test/queue.js
@@ -1,59 +1,77 @@
 'use strict'
-var test = require('tap').test
-var RunQueue = require('../queue.js')
+const test = require('tap').test
+const RunQueue = require('../queue.js')
 
-test('concur', function (t) {
-  t.plan(7)
+test('concur', t => {
+  t.plan(10)
 
-  var queue1 = new RunQueue({
-    maxConcurrency: 1
-  })
-  var finished1 = []
+  const queue1 = new RunQueue()
+  const finished1 = []
   setup(queue1, finished1)
-  var queue1complete = queue1.run()
+  const queue1complete = queue1.run()
   t.is(queue1.run(), queue1complete, 'multiple run calls get the same promise')
-  queue1complete.then(function () {
+  queue1complete.then(() => {
     t.isDeeply(finished1, [ 0, 1, 2, 3, 4, -1 ], 'concurrency 1')
     t.is(queue1.run(), queue1complete, 'multiple run calls get the same promise, even after completion')
   })
 
-  var queue10 = new RunQueue({
+  const queueReprio = new RunQueue({
+    maxConcurrency: 1
+  })
+  const finishedReprio = []
+
+  queueReprio.add(2, () => Promise.resolve(finishedReprio.push(1)))
+  queueReprio.add(2, () => Promise.resolve(finishedReprio.push(2)))
+  queueReprio.add(2, () => {
+    queueReprio.add(1, () => Promise.resolve(finishedReprio.push('a')))
+    queueReprio.add(1, () => Promise.resolve(finishedReprio.push('b')))
+    return Promise.resolve(finishedReprio.push(3))
+  })
+  queueReprio.add(2, () => Promise.resolve(finishedReprio.push(4)))
+  queueReprio.add(2, () => Promise.resolve(finishedReprio.push(5)))
+  queueReprio.run().then(() => {
+    t.isDeeply(finishedReprio, [1,2,3,'a','b',4,5], 'higher prio tasks trump lower, even at runtime')
+  })
+
+  const queue10 = new RunQueue({
     maxConcurrency: 10
   })
-  var finished10 = []
+  const finished10 = []
   setup(queue10, finished10)
-  queue10.run().then(function () {
+  queue10.run().then(() => {
     t.isDeeply(finished10, [ 4, 3, 2, 1, 0, -1 ], 'concurrency 10')
   })
 
-  var queueE = new RunQueue({
+  const queueE = new RunQueue({
     maxConcurrency: 3
   })
-  queueE.add(0, function () {
+  queueE.add(0, () => {
     throw new Error('STOP!')
   })
-  var finishedE = []
+  const finishedE = []
   setup(queueE, finishedE)
-  queueE.run().then(function () {
+  queueE.run().then(() => {
     t.fail('got early error')
-    t.fail()
-    t.fail()
   }, function (err) {
     t.is(err && err.message, 'STOP!', 'got early error')
     t.isDeeply(finishedE, [ ], 'no results on early error')
 
     try {
-      queueE.add(0, function () {}, [])
+      queueE.add(0, () => {}, [])
       t.fail('throw on add to finished queue')
     } catch (ex) {
       t.pass('throw on add to finished queue')
     }
   })
+
+  t.throws(() => queueE.run('abc'))
+
+  t.throws(() => queueE.add(-1))
 })
 
 function setup (queue, finished) {
   queue.add(1, example(finished), [-1])
-  for (var ii = 0; ii < 5; ++ii) {
+  for (let ii = 0; ii < 5; ++ii) {
     queue.add(0, example(finished), [ii])
   }
 }
@@ -61,7 +79,7 @@ function setup (queue, finished) {
 function example (finished) {
   return function (num) {
     return new Promise(function (resolve) {
-      setTimeout(function () {
+      setTimeout(() => {
         finished.push(num)
         resolve()
       }, (5 - Math.abs(num)) * 10)
